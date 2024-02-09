2bec7bb5016c91e1e33db42df862f240d564d3e2
dstorie80 <dstorie80@gmail.com>
2024-02-08 23:24:07 -0600

    Added code for the shape formations

--- assets/js/generateHTML.js
+++ assets/js/generateHTML.js
@@ -4,11 +4,11 @@ const generateHTML = (userInput) => {
     const outPutHTML = 
     `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
-    ${shape === "circle" ? `<circle cx="150" cy="150" r="80" fill="${sColor}" />` : ""}
-    ${shape === "triangle" ? `<polygon points="150,20 100,100 200,100" fill="${sColor}" />` : ""}
-    ${shape === "square" ? `<rect x="100" y="100" width="100" height="100" fill="${sColor}" />` : ""}
+    ${shape === "Circle" ? `<circle cx="150" cy="120" r="80" fill="${sColor}" /> \n <text x="150" y="145" font-size="60" text-anchor="middle" fill="${tColor}">${characters}</text>` : ""}
+    ${shape === "Triangle" ? `<polygon points="150,20 100,100 200,100" fill="${sColor}" /> \n  <text x="150" y="85" font-size="30" text-anchor="middle" fill="${tColor}">${characters}</text>` : ""}
+    ${shape === "Square" ? `<rect x="100" y="100" width="100" height="100" fill="${sColor}" /> \n <text x="150" y="160" font-size="50" text-anchor="middle" fill="${tColor}">${characters}</text>` : ""}
   
-    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${tColor}">${characters}</text>
+    
   
   </svg>`;
 
--- index.js
+++ index.js
@@ -9,31 +9,35 @@ inquirer
             type: 'input',
             message: 'Enter up to 3 alpha characters',
             name: 'characters',
+            validate: function (value) {
+                if (value.length === 3) {
+                    return true;
+                } else {
+                    return 'Please enter 3 characters';
+                }
+            }
         },
         {
             type: 'input',
-            message: 'Enter a hex color code for the background',
+            message: 'Enter a color or hex code for the text color',
             name: 'tColor',
         },
         {
             type: 'list',
             message: 'Select circle, triangle or square',
             name: 'shape',
-            choices:['circle', 'triangle', 'square']
+            choices:['Circle', 'Triangle', 'Square']
         },
         {
             type: 'input',
-            message: 'Enter hex code for shape color',
+            message: 'Enter a color or hex code for shape color',
             name: 'sColor'
         },
     ])
 
     .then((answers) => {
         const fileContents = generateHTML(answers);
-        // const { title } = answers;
-
-        // const fileName = title.toLowerCase().replace(" ", "_") + ".html";
-        fs.writeFile(`logo.svg`, fileContents, (err) => {
+        fs.writeFile(`logo_${answers.shape}.svg`, fileContents, (err) => {
             if (err) {
                 console.error(err);
             } else {
